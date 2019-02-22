d85ed2509be528e11334ab84baa328c916aa65d5
ssenoga <41122179+ssenoga@users.noreply.github.com>
2019-02-22 15:59:05 +0300

    changes to the site

--- final/script.js
+++ final/script.js
@@ -1,25 +1,23 @@
+// class EasyLogin {
+//   get(url){
+//     fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => data);
+//   }
+// }
 
-
-const demo = document.querySelector('#demo');
-const xml = new XMLHttpRequest();
-xml.open("GET","http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id='4d463112'&app_key='b0890a43c6ba84a3ae201a52ab731e6f'",true);
-xml.onload = function(){
-    if(this.status === 200){
-        const tableBody = document.querySelector('#tableBody');
-        // const tr = document.createElement('tr');
-        const api = JSON.parse(this.responseText);
-        let items = ``;
-        api.forEach(function(item){
-            items += `<tr>
-               <td>${item.id}</td>
-               <td>${item.title}</td>
-               <td>${item.body}</td>
-               </tr>
-            `;
-        });
-        tableBody.innerHTML = items;
-
-    }
+const EasyLogin = function(http){
+  this.http = new XMLHttpRequest();
 }
+// get
+EasyLogin.prototype.get = function(url){
+  this.http.open('GET',url,true);
+  const self = this;
+  this.http.onload = function(){
+    if(self.http.status === 200){
+      console.log(self.http.textResponse);
+    }
+  }
 
-xml.send();
+  this.http.send();
+}
