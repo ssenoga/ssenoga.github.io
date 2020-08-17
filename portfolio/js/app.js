6e9b1876792b4b97c5ec35f9a38bcf50cb20b956
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-17 14:08:46 +0300

    Update app.js

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./serviceWorker.js")
+      .register("serviceWorker.js")
       .then((reg) => console.log("sucess"))
       .catch((err) => console.log(`Service Worker Error: ${err}`));
   });
