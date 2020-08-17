de8736b223052fcb1df1be2036ede4e97d210bec
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-17 13:06:23 +0300

    Add files via upload

---
+++ portfolio/js/app.js
@@ -0,0 +1,9 @@
+//check to see if service worker is supported
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../serviceWorker.js")
+      .then((reg) => console.log("sucess"))
+      .catch((err) => console.log(`Service Worker Error: ${err}`));
+  });
+}
